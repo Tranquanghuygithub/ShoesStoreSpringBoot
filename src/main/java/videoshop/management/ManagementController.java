4bb6919b3e80d2d8cd7e34282b9031de9908a989
jkaicoder <jkaicoder@gmail.com>
2019-06-06 16:22:46 +0700

    Update management [customer, product, orders]

--- src/main/java/videoshop/management/ManagementController.java
+++ src/main/java/videoshop/management/ManagementController.java
@@ -2,26 +2,42 @@ package videoshop.management;
 
 import org.salespointframework.inventory.Inventory;
 import org.salespointframework.inventory.InventoryItem;
+import org.salespointframework.order.Order;
+import org.salespointframework.order.OrderManager;
+import org.salespointframework.order.OrderStatus;
+import org.springframework.data.util.Streamable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import videoshop.customer.Customer;
+import videoshop.customer.CustomerManagement;
+import videoshop.shoes.catalog.Shoes;
+
 
 @Controller
 @RequestMapping("/management")
 public class ManagementController {
 	private final Inventory<InventoryItem> inventory;
+	
+	private final OrderManager<Order> orderManager;
+	private final CustomerManagement customerManagement;
 
-	ManagementController(Inventory<InventoryItem> inventory) {
+	ManagementController(Inventory<InventoryItem> inventory, OrderManager<Order> orderManager, CustomerManagement customerManagement) {
 		this.inventory = inventory;
+		Assert.notNull(orderManager, "OrderManager must not be null!");
+		this.orderManager = orderManager;
+		this.customerManagement = customerManagement;
 	}
 	
 	@GetMapping("/shoesStock")
 	//@PreAuthorize("hasRole('BOSS')")
 	String ShoesStock(Model model) {
 
-		//model.addAttribute("stock", inventory.findAll());
+		model.addAttribute("stock", inventory.findAll());
 
 		return "shoesstock";
 	}
@@ -31,6 +47,10 @@ public class ManagementController {
 	String CustomerManagement(Model model) {
 
 		//model.addAttribute("stock", inventory.findAll());
+		
+		model.addAttribute("customerList", customerManagement.findAll());
+		
+		
 
 		return "customer_management";
 	}
@@ -40,6 +60,8 @@ public class ManagementController {
 	String OrdersManagement(Model model) {
 
 		//model.addAttribute("stock", inventory.findAll());
+		
+		model.addAttribute("ordersCompleted", orderManager.findBy(OrderStatus.COMPLETED));
 
 		return "orders_management";
 	}
@@ -52,4 +74,6 @@ public class ManagementController {
 
 		return "add_shoes";
 	}
+	
+	
 }
